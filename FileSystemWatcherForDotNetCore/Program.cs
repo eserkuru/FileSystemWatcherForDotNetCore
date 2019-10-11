d9104cd046666ed8c6dde76fe05ac98a09bd7e46
Eser Kuru <eserkuru@users.noreply.github.com>
2019-10-11 13:02:50 +0300

    Initial commit

---
+++ FileSystemWatcherForDotNetCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystemWatcherForDotNetCore
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"D:\FSW";
+            MonitorDirectory(path);
+            Console.ReadKey();
+        }
+
+        private static void MonitorDirectory(string path)
+        {
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher {Path = path};
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            var sw = new StreamWriter(@"D:\FileWatcherLog.txt", true);
+            sw.WriteLine("{0} {1} file was created.", DateTime.Now, e.Name);
+            sw.Close();
+        }
+
+    }
+}
